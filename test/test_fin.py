e6accde4376ea86361cc3a45eb8c99bc510f396a
Mindey <mindey@qq.com>
2016-11-14 06:41:55 +0000

    fixed tests, and gitignore

--- test/test_fin.py
+++ test/test_fin.py
@@ -1,18 +1,17 @@
 import unittest
-from datetime import date
+import datetime
 
 import fin
-
 
 class BasicTest(unittest.TestCase):
     def test_xirr(self):
 
-        test_dates = [date(2010, 12, 29), date(2012, 1, 25), date(2012, 3, 8)]
+        test_dates = [datetime.date(2010, 12, 29), datetime.date(2012, 1, 25), datetime.date(2012, 3, 8)]
         test_values = [-10000, 20, 10100]
 
         result = 0.010061265164920336
 
-        self.assertEqual(fin.xirr(test_dates, test_values), result)
+        self.assertEqual(fin.xirr(test_values, test_dates), result)
 
 if __name__ == '__main__':
     unittest.main()
